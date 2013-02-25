f9ac3c9bb1beb8081f88a6c0e892a19e1eb97883
Quentin CERNY <qcerny.extia@tf1.fr>
2013-02-25 15:04:33 +0100

    removes generated files

--- src/EasyTask/Bundle/LogBundle/Model/LogQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace EasyTask\Bundle\LogBundle\Model;
-
-use EasyTask\Bundle\LogBundle\Model\om\BaseLogQuery;
-
-class LogQuery extends BaseLogQuery
-{
-}
--- src/EasyTask/Bundle/WorkflowBundle/Model/Task.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace EasyTask\Bundle\WorkflowBundle\Model;
-
-use EasyTask\Bundle\WorkflowBundle\Model\om\BaseTask;
-
-class Task extends BaseTask
-{
-}
