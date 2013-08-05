ec649df128ce97c2ac1684d49145e45e6326e288
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-08-05 19:08:33 +0200

    add new route and update template

--- src/Extia/Bundle/UserBundle/Controller/ManagementController.php
+++ src/Extia/Bundle/UserBundle/Controller/ManagementController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Extia\Bundle\UserBundle\Controller;
 
+use Extia\Bundle\UserBundle\Model\ConsultantQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -19,7 +20,7 @@ class ManagementController extends Controller
      * @return array
      * @Template("ExtiaUserBundle:Management:edit")
      */
-    public function addAction(Request $request)
+    public function createAction(Request $request)
     {
         $form = $this->get('extia_user.person_form');
 
@@ -40,12 +41,75 @@ class ManagementController extends Controller
     }
 
     /**
-     * @param Request $request
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param                                           $id
      * @Template()
      */
-    public function deleteAction(Request $request)
+    public function deleteCrhAction(Request $request, $id)
     {
+        $user = PersonQuery::create()
+            ->find();
 
+        if ($user) {
+            try {
+                $user->delete();
+                $this->get('session')->setFlash('notice', 'La création du nouvel utilisateur est valide.');
+                $this->redirect($this->generateUrl('extia_user_management_delete_crh'));
+            } catch (\Exception $e) {
+                $this->get('session')->setFlash('error', 'Impossible de supprimer le compte utilisateur.');
+
+            }
+        }
+        $this->get('session')->setFlash('error', 'Aucun compte utilisateur à supprimer.');
+        $this->redirect($this->generateUrl('extia_user_management_list_crh'));
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param                                           $id
+     * @Template()
+     */
+    public function deleteManagerAction(Request $request, $id)
+    {
+        $user = PersonQuery::create()
+            ->findById($id);
+
+        if ($user) {
+            try {
+                $user->delete();
+                $this->get('session')->setFlash('notice', 'La création du nouvel utilisateur est valide.');
+                $this->redirect($this->generateUrl('extia_user_management_delete_manager'));
+            } catch (\Exception $e) {
+                $this->get('session')->setFlash('error', 'Impossible de supprimer le compte utilisateur.');
+
+            }
+        }
+        $this->get('session')->setFlash('error', 'Aucun compte utilisateur à supprimer.');
+        $this->redirect($this->generateUrl('extia_user_management_list_manager'));
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param                                           $id
+     *
+     * @Template()
+     */
+    public function deleteConsultantAction(Request $request, $id)
+    {
+        $user = ConsultantQuery::create()
+            ->findById($id);
+
+        if ($user) {
+            try {
+                $user->delete();
+                $this->get('session')->setFlash('notice', 'La création du nouvel utilisateur est valide.');
+                $this->redirect($this->generateUrl('extia_user_management_delete_consultant'));
+            } catch (\Exception $e) {
+                $this->get('session')->setFlash('error', 'Impossible de supprimer le compte utilisateur.');
+            }
+        }
+        $this->get('session')->setFlash('error', 'Aucun compte utilisateur à supprimer.');
+        $this->redirect($this->generateUrl('extia_user_management_list_consultant'));
     }
 
     /**
@@ -66,6 +130,7 @@ class ManagementController extends Controller
 
         if ($process) {
             $this->get('session')->setFlash('notice', 'La création du nouvel utilisateur est valide.');
+            $this->redirect($this->generateUrl('Rhea_homepage'));
         }
 
         return array (
@@ -73,5 +138,29 @@ class ManagementController extends Controller
             'edit'     => true,
             'hasError' => $request->getMethod() == 'POST' && !$form->isValid()
         );
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @Tamplate()
+     */
+    public function listConsultantAction(Request $request) {
+
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @Tamplate("ExtiaUSerBundle:Management:list.html.twig")
+     */
+    public function listManagerAction(Request $request) {
+
+    }
+
+    /**
+     * @param Request $request
+     * @Tamplate("ExtiaUSerBundle:Management:list.html.twig")
+     */
+    public function listCrhAction(Request $request) {
+
     }
 }
