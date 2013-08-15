ad44f4362eb6db541b5914cf2d7c0938b1879884
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-08-15 15:54:49 +0200

    add schema propel and move definition table propel

---
+++ src/Extia/Bundle/MissionBundle/Model/Mission.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Extia\Bundle\MissionBundle\Model;
+
+use Extia\Bundle\MissionBundle\Model\om\BaseMission;
+
+class Mission extends BaseMission
+{
+}
---
+++ src/Extia/Bundle/MissionBundle/Model/MissionQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Extia\Bundle\MissionBundle\Model;
+
+use Extia\Bundle\MissionBundle\Model\om\BaseMissionQuery;
+
+class MissionQuery extends BaseMissionQuery
+{
+}
