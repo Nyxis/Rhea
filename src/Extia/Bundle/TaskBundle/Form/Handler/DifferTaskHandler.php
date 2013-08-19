7481134a7ed2efb8777f8d5d06916c0480827d74
QuentinCerny <quentin.cerny@gmail.com>
2013-08-19 22:26:27 +0200

    adds date changing process for all workflows

--- src/Extia/Bundle/TaskBundle/Form/Handler/DifferTaskHandler.php
+++ src/Extia/Bundle/TaskBundle/Form/Handler/DifferTaskHandler.php
@@ -3,13 +3,15 @@
 namespace Extia\Bundle\TaskBundle\Form\Handler;
 
 use Extia\Bundle\TaskBundle\Model\TaskQuery;
+use Extia\Bundle\TaskBundle\Workflow\Aggregator;
 
 use Extia\Bundle\CommentBundle\Model\Comment;
 use Extia\Bundle\NotificationBundle\Notification\NotifierInterface;
 
+use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * handler for task differing
@@ -17,17 +19,20 @@ use Symfony\Component\Templating\EngineInterface;
  */
 class DifferTaskHandler
 {
-    protected $templateEngine;
+    protected $workflows;
+    protected $securityContext;
+    protected $translator;
     protected $notifier;
 
     /**
      * construct
-     * @param EngineInterface $engine
      */
-    public function __construct(EngineInterface $engine, NotifierInterface $notifier)
+    public function __construct(Aggregator $workflows, SecurityContextInterface $securityContext, TranslatorInterface $translator, NotifierInterface $notifier)
     {
-        $this->templateEngine = $engine;
-        $this->notifier       = $notifier;
+        $this->workflows       = $workflows;
+        $this->securityContext = $securityContext;
+        $this->translator  = $translator;
+        $this->notifier        = $notifier;
     }
 
     /**
@@ -38,6 +43,60 @@ class DifferTaskHandler
      */
     public function handle(Form $form, Request $request)
     {
+        $form->submit($request);
 
+        if (!$form->isValid()) {
+            $this->notifier->add('warning', 'task.differ.notification.invalid');
+            return;
+        }
+
+        $data = $form->getData();
+
+        try {
+            $internal = $this->securityContext->getToken()->getUser();
+            $teamIds  = $internal->getTeamIds();
+
+            // me and my team
+            $teamIds = empty($teamIds) ? array($internal->getId()) : $teamIds->prepend($internal->getId())->getData();
+
+            $task = TaskQuery::create()
+                ->setComment(sprintf('%s l:%s', __METHOD__, __LINE__))
+                ->filterByWorkflowTypes(array_keys($this->workflows->getAllowed('write')))
+                ->filterByAssignedTo($teamIds)
+                ->findPk($data['task_id']);
+
+            if (empty($task)) {
+                $this->notifier->add('warning', 'task.differ.notification.invalid');
+                return;
+            }
+
+            // modify date
+            $oldDate = $task->getActivationDate();
+            $task->setActivationDate($task->findNextWorkingDay($data['differ_date']));
+            $task->getNode()->getType()->onTaskDiffering($task);
+
+            // create comment
+            $transParams = array(
+                '%new_date%' => $task->getActivationDate($this->translator->trans('date_format.ymd')),
+                '%old_date%' => $oldDate->format($this->translator->trans('date_format.ymd')),
+                '%task%'     => $task->getNode()->getWorkflow()->getName()
+            );
+
+            $comment = new Comment();
+            $comment->setWrittenBy($internal->getId());
+            $comment->setTask($task);
+            $comment->setText($data['comment']);
+            $comment->setSysText($this->translator->trans('task.differ.comment.auto_message', $transParams));
+
+            $task->save();
+
+            $this->notifier->add('success', 'task.differ.notification.success', $transParams);
+
+        } catch (\Exception $e) {
+            $this->notifier->add('error', 'task.differ.notification.error');
+
+            var_dump($e->getMessage());
+            die;
+        }
     }
 }
