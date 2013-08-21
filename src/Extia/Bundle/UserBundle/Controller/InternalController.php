8553383538eeda51fef84c4d8d362654b1dd6430
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-08-21 16:47:03 +0200

    fix bug : error for generate routing in to edition my team

--- src/Extia/Bundle/UserBundle/Controller/InternalController.php
+++ src/Extia/Bundle/UserBundle/Controller/InternalController.php
@@ -166,7 +166,7 @@ class InternalController extends Controller
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      * @return Response
      */
-    public function editAction(Request $request, $id)
+    public function editAction(Request $request, $Id)
     {
         $internal = InternalQuery::create()
             ->setComment(sprintf('%s l:%s', __METHOD__, __LINE__))
@@ -174,10 +174,10 @@ class InternalController extends Controller
             ->useJobQuery()
                 ->joinWithI18n()
             ->endUse()
-            ->findPk($id);
+            ->findPk($Id);
 
         if (empty($internal)) {
-            throw new NotFoundHttpException(sprintf('Any consultant found for given id, "%s" given.', $id));
+            throw new NotFoundHttpException(sprintf('Any consultant found for given id, "%s" given.', $Id));
         }
 
         $form = $this->renderForm($request, $internal);
