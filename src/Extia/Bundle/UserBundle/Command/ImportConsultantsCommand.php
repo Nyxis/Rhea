48f7bf6f63c8363f864e82a36155b94d47bcdfe8
Quentin Cerny <quentin.cerny@ma-residence.fr>
2013-10-28 17:43:58 +0100

    adds internal and consultant record insertion logic from csvs

--- src/Extia/Bundle/UserBundle/Command/ImportConsultantsCommand.php
+++ src/Extia/Bundle/UserBundle/Command/ImportConsultantsCommand.php
@@ -2,8 +2,14 @@
 
 namespace Extia\Bundle\UserBundle\Command;
 
+use Extia\Bundle\UserBundle\Model\Consultant;
+use Extia\Bundle\UserBundle\Model\ConsultantQuery;
 use Extia\Bundle\UserBundle\Model\Internal;
+use Extia\Bundle\UserBundle\Model\InternalQuery;
+use Extia\Bundle\UserBundle\Model\PersonQuery;
 use Extia\Bundle\UserBundle\Model\PersonTypeQuery;
+
+use Extia\Bundle\GroupBundle\Model\GroupQuery;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
@@ -12,17 +18,17 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Command to launch for import internals from a csv file
+ * Command to launch for import users from a csv file
  */
-class ImportInternalsCommand extends ContainerAwareCommand
+class ImportConsultantsCommand extends ContainerAwareCommand
 {
     /**
      * @see ContainerAwareCommand::configure()
      */
     protected function configure()
     {
-        $this->setName("extia:internals:import")
-            ->setDescription("Imports internals from given file")
+        $this->setName("extia:consultants:import")
+            ->setDescription("Imports mission from given file")
             ->addArgument('csv', InputArgument::REQUIRED, 'Consultant data you want to import')
         ;
     }
@@ -45,60 +51,134 @@ class ImportInternalsCommand extends ContainerAwareCommand
             ));
         }
 
+        // ConsultantQuery::create()->deleteAll();
+        // InternalQuery::create()->deleteAll();
+        // PersonQuery::create()->deleteAll();
+
         // for multiple rows
-        $internalsList = array();
+        $consultantList = array();
 
         $handle    = fopen($file, 'r');
         $firstLine = fgetcsv($handle);
         while ($data = fgetcsv($handle)) {
-            $data = array(
-                'lastname'            => $data[0],
-                'firstname'           => $data[1],
-                'fullname'            => $data[2],
-                'birthdate'           => $data[3],
-                'email'               => $data[4],
-                'phone'               => $data[5],
-                'contract_begin_date' => $data[6],
-                'parent'              => $data[7],
-                'type'                => $data[8],
+            $indexedData = array(
+                'period'              => $data[0],
+                'lastname'            => $data[1],
+                'firstname'           => $data[2],
+                'fullname'            => $data[3],
+                'birthdate'           => $data[4],
+                'email'               => $data[5],
+                'mobile'              => $data[6],
+                'contract_begin_date' => $data[7],
+                'adp'                 => $data[8],
+                'crh_lastname'        => $data[9],
+                'crh_firstname'       => $data[10],
+                'contract_type'       => $data[11],
+                'mission_id'          => $data[12],
+                'client_name'         => $data[13],
+                'manager_trigram'     => $data[14],
+                'manager_lastname'    => $data[15],
+                'manager_firstname'   => $data[16],
+                'debut_mission'       => $data[17],
+                'fin_mission'         => $data[18]
             );
 
-            if (!in_array($data['type'], array('COMMERCIAL', 'DIRECTION', 'RESSOURCES HUMAINES'))) {
+            if (empty($indexedData['email']) || $indexedData['email'] == '#N/A') {
                 continue;
             }
 
-            $internalsList[] = $data;
+            // already exists, only adds mission
+            $email = $indexedData['email'];
+            if (!isset($consultantList[$email])) {
+                $consultantList[$email] = array_intersect_key(
+                    $indexedData,
+                    array_flip(array('lastname', 'firstname', 'fullname', 'birthdate', 'email', 'mobile', 'contract_begin_date', 'crh_lastname', 'crh_firstname', 'contract_type'))
+                );
+
+                $consultantList[$email]['missions'] = array();
+            }
+
+            $consultantList[$indexedData['email']]['missions'][] = array_intersect_key(
+                $indexedData,
+                array_flip(array('client_name', 'manager_trigram', 'manager_lastname', 'manager_firstname', 'debut_mission', 'fin_mission'))
+            );
         }
         fclose($handle);
 
-        // all person type
-        $personTypes = PersonTypeQuery::create()
-            ->filterByCode(array('pdg', 'ia', 'crh'))
-            ->find()
-            ->toKeyValue('Code', 'Id')
+
+        // consultant person type
+        $personType = PersonTypeQuery::create()
+            ->filterByCode(array('clt'))
+            ->findOne()
         ;
 
-        $typeMap = array(
-            'COMMERCIAL'          => $personTypes['ia'],
-            'DIRECTION'           => $personTypes['pdg'],
-            'RESSOURCES HUMAINES' => $personTypes['crh']
-        );
+        // consultant group
+        $group = GroupQuery::create()
+            ->filterByLabel(array('Consultant'))
+            ->findOne()
+        ;
 
+        // tmp manager (calculated with missions)
+        $tmpManager = InternalQuery::create()
+            ->usePersonTypeQuery()
+                ->filterByCode('ia')
+            ->endUse()
+            ->findOne()
+        ;
 
-        // first, insert all internals, without setting any tree values
-        foreach ($internalsList as $internalData) {
-            $internal = new Internal();
-            $internal
+        // first : create consultant
+        foreach ($consultantList as $consultantData) {
+            $consultant = new Consultant();
+            $consultant->setPersonType($personType);
+            $consultant->setGroup($group);
+
+            $consultant->setFirstname(ucfirst(strtolower($consultantData['firstname'])));
+            $consultant->setLastname(ucfirst(strtolower($consultantData['lastname'])));
+            $consultant->setEmail($consultantData['email']);
+            $consultant->setMobile($consultantData['mobile']);
+
+            $consultant->setBirthdate(
+                \DateTime::createFromFormat('d/m/y', $consultantData['birthdate'])
+            );
+            $consultant->setContractBeginDate(
+                \DateTime::createFromFormat('d/m/y', $consultantData['contract_begin_date'])
+            );
+
+            // find crh
+            $consultant->setCrh(InternalQuery::create()
+                ->filterByFirstname($consultantData['crh_firstname'])
+                ->filterByLastname($consultantData['crh_lastname'])
+                ->findOne()
+            );
+
+            // tmp manager
+            $consultant->setManager($tmpManager);
+
+            $consultant->save();
+
+            $output->writeln(sprintf('%s %s %s',
+                $consultant->getFirstname(),
+                $consultant->getLastName(),
+                $consultant->getEmail()
+            ));
+        }
+
+        // create missions and orders
+        foreach ($consultantList as $consultantData) {
+            $consultant = ConsultantQuery::create()
+                ->findOneByEmail($consultantData['email'])
+            ;
+
+            // missions and orders
+            foreach ($consultantData['missions'] as $mission) {
+                var_dump($mission);
+            }
         }
 
 
 
-        // then, update tree with parent data
-
-
-
-        foreach ($consultantList as $consultant) {
-            $output->writeln($consultant['email']);
-        }
     }
 }
+
+
+
--- src/Extia/Bundle/UserBundle/Command/ImportInternalsCommand.php
+++ src/Extia/Bundle/UserBundle/Command/ImportInternalsCommand.php
@@ -2,6 +2,13 @@
 
 namespace Extia\Bundle\UserBundle\Command;
 
+use Extia\Bundle\UserBundle\Model\Internal;
+use Extia\Bundle\UserBundle\Model\InternalQuery;
+use Extia\Bundle\UserBundle\Model\PersonQuery;
+use Extia\Bundle\UserBundle\Model\PersonTypeQuery;
+
+use Extia\Bundle\GroupBundle\Model\GroupQuery;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 use Symfony\Component\Console\Input\InputInterface;
@@ -9,17 +16,17 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Command to launch for import users from a csv file
+ * Command to launch for import internals from a csv file
  */
-class ImportConsultantsCommand extends ContainerAwareCommand
+class ImportInternalsCommand extends ContainerAwareCommand
 {
     /**
      * @see ContainerAwareCommand::configure()
      */
     protected function configure()
     {
-        $this->setName("extia:consultants:import")
-            ->setDescription("Imports mission from given file")
+        $this->setName("extia:internals:import")
+            ->setDescription("Imports internals from given file")
             ->addArgument('csv', InputArgument::REQUIRED, 'Consultant data you want to import')
         ;
     }
@@ -42,58 +49,138 @@ class ImportConsultantsCommand extends ContainerAwareCommand
             ));
         }
 
+        InternalQuery::create()->deleteAll();
+        PersonQuery::create()->deleteAll();
+
         // for multiple rows
-        $consultantList = array();
+        $internalsList = array();
 
         $handle    = fopen($file, 'r');
         $firstLine = fgetcsv($handle);
         while ($data = fgetcsv($handle)) {
-            $indexedData = array(
-                'period'              => $data[0],
-                'lastname'            => $data[1],
-                'firstname'           => $data[2],
-                'fullname'            => $data[3],
-                'birthdate'           => $data[4],
-                'email'               => $data[5],
-                'mobile'              => $data[6],
-                'contract_begin_date' => $data[7],
-                'adp'                 => $data[8],
-                'crh_lastname'        => $data[9],
-                'crh_firstname'       => $data[10],
-                'contract_type'       => $data[11],
-                'mission_id'          => $data[12],
-                'client_name'         => $data[13],
-                'manager_trigram'     => $data[14],
-                'manager_lastname'    => $data[15],
-                'manager_firstname'   => $data[16],
-                'debut_mission'       => $data[17],
-                'fin_mission'         => $data[18]
+            $data = array(
+                'lastname'            => $data[0],
+                'firstname'           => $data[1],
+                'fullname'            => $data[2],
+                'birthdate'           => $data[3],
+                'email'               => $data[4],
+                'phone'               => $data[5],
+                'contract_begin_date' => $data[6],
+                'parent'              => $data[7],
+                'type'                => $data[8],
             );
 
-            if (empty($indexedData['email']) || $indexedData['email'] == '#N/A') {
+            if (!in_array($data['type'], array('COMMERCIAL', 'DIRECTION', 'RESSOURCES HUMAINES'))) {
                 continue;
             }
 
-            // already exists, only adds mission
-            $email = $indexedData['email'];
-            if (!isset($consultantList[$email])) {
-                $consultantList[$email] = array_intersect_key(
-                    $indexedData,
-                    array_flip(array('lastname', 'firstname', 'fullname', 'birthdate', 'email', 'mobile', 'contract_begin_date', 'crh_lastname', 'crh_firstname', 'contract_type'))
-                );
-
-                $consultantList[$email]['missions'] = array();
-            }
-
-            $consultantList[$indexedData['email']]['missions'][] = array_intersect_key(
-                $indexedData,
-                array_flip(array('client_name', 'manager_trigram', 'manager_lastname', 'manager_firstname', 'debut_mission', 'fin_mission'))
-            );
+            $internalsList[] = $data;
         }
         fclose($handle);
 
-        foreach ($consultantList as $consultant) {
-            $output->writeln($consultant['email']);
+        // all person type
+        $personTypes = PersonTypeQuery::create()
+            ->filterByCode(array('pdg', 'ia', 'crh'))
+            ->find()
+            ->toKeyValue('Code', 'Id')
+        ;
+
+        $typeMap = array(
+            'COMMERCIAL'          => $personTypes['ia'],
+            'DIRECTION'           => $personTypes['pdg'],
+            'RESSOURCES HUMAINES' => $personTypes['crh']
+        );
+
+        // groups
+        $groups = GroupQuery::create()
+            ->filterByLabel(array('IA - Manager', 'Crh'))
+            ->find()
+            ->toKeyValue('Label', 'Id')
+        ;
+
+        $groupMap = array(
+            'COMMERCIAL'          => $groups['IA - Manager'],
+            'DIRECTION'           => $groups['IA - Manager'],
+            'RESSOURCES HUMAINES' => $groups['Crh']
+        );
+
+        // first, insert all internals, without setting any tree values
+        foreach ($internalsList as $internalData) {
+            $internal = new Internal();
+            $internal->setPersonTypeId($typeMap[$internalData['type']]);
+            $internal->setGroupId($groupMap[$internalData['type']]);
+
+            $internal->setFirstname(ucfirst(strtolower($internalData['firstname'])));
+            $internal->setLastname(ucfirst(strtolower($internalData['lastname'])));
+            $internal->setEmail($internalData['email']);
+            $internal->setBirthdate(
+                \DateTime::createFromFormat('d/m/y', $internalData['birthdate'])
+            );
+            $internal->setContractBeginDate(
+                \DateTime::createFromFormat('d/m/y', $internalData['contract_begin_date'])
+            );
+
+            $internal->setTrigram(strtoupper(sprintf('%s%s',
+                substr($internalData['firstname'], 0, 1),
+                substr($internalData['lastname'], 0, 2)
+            )));
+
+            if ((int) substr($internalData['phone'], 1, 2) >= 6) {
+                $internal->setMobile($internalData['phone']);
+            }
+            else {
+                $internal->setTelephone($internalData['phone']);
+            }
+
+            // make root on empty parent
+            if (empty($internalData['parent'])) {
+                $internal->makeRoot();
+            }
+            else {
+                $internal->setTreeLeft(0);
+                $internal->setTreeRight(0);
+                $internal->setTreeLevel(1);
+            }
+
+            $internal->save();
+
+            $output->writeln(sprintf('%s %s %s %s',
+                $internal->getTrigram(),
+                $internal->getFirstname(),
+                $internal->getLastName(),
+                $internal->getEmail()
+            ));
+        }
+
+        // then, update tree with parent data
+        foreach ($internalsList as $internalData) {
+            if (empty($internalData['parent'])) {
+                continue;
+            }
+
+            $internal = InternalQuery::create()
+                ->findOneByEmail($internalData['email'])
+            ;
+            $parent = InternalQuery::create()
+                ->findOneByTrigram($internalData['parent'])
+            ;
+
+            if (empty($parent)) {
+                $output->writeln(sprintf('WARNING :: %s@%s does not exists',
+                    $internal->getTrigram(),
+                    $internalData['parent']
+                ));
+
+                continue;
+            }
+
+            $internal->insertAsLastChildOf($parent);
+            $internal->save();
+
+            $output->writeln(sprintf('%s << %s',
+                $parent->getTrigram(),
+                $internal->getTrigram()
+            ));
         }
     }
 }
