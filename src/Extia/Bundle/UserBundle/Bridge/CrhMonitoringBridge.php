6af0f57ee407faf127f31cbf40bf8a443c76d8b9
QuentinCerny <quentin.cerny@gmail.com>
2013-10-28 08:30:19 +0100

    refacto some consultants domains methods, adds bridge for old wfs, adds command for imports

---
+++ src/Extia/Bundle/UserBundle/Bridge/CrhMonitoringBridge.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Extia\Bundle\UserBundle\Bridge;
+
+use Extia\Bundle\UserBundle\Model\Consultant;
+
+/**
+ * bridge to crh monitoring bundle
+ *
+ * @see Extia/Bundles/UserBundle/Resources/config/bridges.xml
+ */
+class CrhMonitoringBridge extends AbstractTaskBridge
+{
+    /**
+     * @see AbstractTaskBridge::getBridgedWorkflow()
+     */
+    protected function getBridgedWorkflow()
+    {
+        return 'crh_monitoring';
+    }
+
+    /**
+     * creates and init mission monitoring for given consultant
+     *
+     * @param Consultant $consultant
+     * @param string     $nextDate   next meeting date (US format) - optionnal
+     * @param \Pdo       $pdo
+     */
+    public function createMonitoring(Consultant $consultant, $nextDate = null, \Pdo $pdo = null)
+    {
+        $currentTask  = $this->createWorkflow(array(), $pdo);
+
+        // bootstrap task
+        return $this->resolveNode($currentTask, array(
+
+                // workflow data
+                'workflow' => array(
+                    'name' => $this->translator->trans('crh_monitoring.default_name', array(
+                        '%user_target%' => $consultant->getLongName()
+                    )),
+                    'description' => $this->translator->trans('crh_monitoring.default_desc', array(
+                        '%user_target%' => $consultant->getLongName()
+                    ))
+                ),
+
+                // bootstrap data
+                'user_target_id' => $consultant->getId(),
+                'assigned_to'    => $consultant->getCrh($pdo),
+                'next_date'      => $currentTask->findNextWorkingDay(empty($nextDate) ?
+                        (int) $currentTask->calculateDate($consultant->getContractBeginDate(), '+1 month', 'U') :
+                        $nextDate
+                )
+            ),
+            $pdo
+        );
+    }
+}
